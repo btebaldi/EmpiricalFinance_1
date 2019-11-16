e5ce3f23ac20d5d93e702cc44eb3c0d8fe57b246
Teo <btebaldi@deskpaper.com>
2019-11-15 21:59:06 -0300

    removido modelo 252, adicionando moving window

--- Trabalho/Script6_RealizedVol_Forecast.r
+++ Trabalho/Script6_RealizedVol_Forecast.r
@@ -21,159 +21,107 @@ Ibov.HF_data.Wide = Ibov.HF_data.Wide %>% dplyr::select(Data, VR, VOLR)
 Ibov.data = dplyr::inner_join(Ibov.data, Ibov.HF_data.Wide, by = c("Date" = "Data"))
 rm(list = c("Ibov.HF_data.Wide"))
 
+summary(Ibov.data)
 
 
-# Declara uma funcao para extrair e calcular os erros de previsao
-GetPredError = function(fit.model, realizedVol.data){
-  
-  # table with errors
-  tb_PredError = tibble(Model = 1:length(fit.model),  fit = NA, MSE = NA, QL = NA)
-  
-  # cylce models
-  for (i in 1:length(fit.model)) {
-    cat(sprintf("Calculating prediction error for: %s\n", names(fit.model[i])))
-    
-    # Get the predicitons and realized values.
-    result = tryCatch({
-      preds <- as.data.frame(fit.model[[i]])
-      TRUE
-    }, warning = function(w) {
-      print(paste("MY_WARNING:  ",war))
-      
-      return(TRUE)
-    }, error = function(e) {
-      print(paste("MY_ERROR:  ", e))
-      
-      return(FALSE)
-    }, finally = {
-      
-    })
-    
-  if (result){
-    
-    preds$date = zoo::as.Date(row.names(preds))
-      dplyr::inner_join(preds, realizedVol.data, by = c("date" = "Data"))
-    
-    
-    
-      # class(fit.model[[i]])
-      # Prediction error for the mean
-      e <- preds$Realized - preds$Mu
-      
-      # Prediction error for the variance
-      d <- e^2 - preds$Sigma^2
-      
-      q <- e^2/preds$Sigma^2 - log(e^2/preds$Sigma^2)
-      
-      # Store prediction error in table
-      tb_PredError[i, "MSE"] = mean(d^2)
-      tb_PredError[i, "QL"] = mean(q)
-    }
-    tb_PredError[i, "fit"] = names(fit.mov5_252[i])
-  }
-  
-  # Return table
-  return(tb_PredError)
+# *************** MOVING WINDOW (3 year) ***************
+# 2003-01-01 = 738
+n.startForecast = nrow(Ibov.data %>% dplyr::filter(Date < "2003-01-01")) + 1
+
+# Reconstroi a serie XTS, pois agora vamos usar roling window forecast
+r_ibov.xts = xts::xts(Ibov.data$r_ibov, order.by = Ibov.data$Date)
+
+# Determina qual o periodo de reestimacao
+# Expanding window recalculation every 5 days
+cat(sprintf("Time frame: 3 years\nRefit every 5 days\n"))
+fit.roll_5 = vector(mode = "list", length = length(models))
+names(fit.roll_5) = names(models)
+for (i in 1:length(models)) {
+  cat(sprintf("Fitting: %s\n", names(fit.roll_5[i])))
+  fit.roll_5[[i]] = ugarchroll(spec = models[[i]],
+                               data = r_ibov.xts,
+                               n.start = n.startForecast,
+                               refit.window = "recursive",
+                               refit.every = 5)
 }
 
-# Lista todos os modelos que serão comparados
-variableNames = c("fit.roll_5", "fit.roll_20", "fit.roll_60", "fit.roll_252",
-                  "fit.mov2_5", "fit.mov2_20", "fit.mov2_60", "fit.mov2_252",
-                  "fit.mov5_5", "fit.mov5_20", "fit.mov5_60", "fit.mov5_252")
-
-# Calcula o erro de previsao para todos os modelos e aagrupa os dados em uma tabela
-tbl = NULL
-for (str in variableNames) {
-  # busca a variavel no environment
-  mdl = get(str)
-  cat(sprintf("Model: %s\n", str))
-  tbl_2 = GetPredError(mdl)
-  str_split = strsplit(str, "_")
-  tbl_2$EstimationName = str_split[[1]][1]
-  tbl_2$Restimation = as.numeric(str_split[[1]][2])
-  tbl = rbind(tbl, tbl_2)
+# Expanding window recalculation every 20 days
+cat(sprintf("Time frame: 3 years\nRefit every 20 days\n"))
+fit.roll_20 = vector(mode = "list", length = length(models))
+names(fit.roll_20) = names(models)
+for (i in 1:length(models)) {
+  cat(sprintf("Fitting: %s\n", names(fit.roll_20[i])))
+  fit.roll_20[[i]] = ugarchroll(spec = models[[i]],
+                                data = r_ibov.xts,
+                                n.start = n.startForecast,
+                                refit.window = "recursive",
+                                refit.every = 20)
 }
-rm(list = c("str", "tbl_2", "mdl", "str_split"))
 
-
-
-
-
-
-
-
-
-
-
-
-tbl$MSE = tbl$MSE * 1e7
-
-for (i in 1:nrow(tbl)) {
-  tbl[i,c("ModelName", "Distribution")] = unlist(strsplit(tbl[[i, "fit"]], "\\."))    
+# Expanding window recalculation every 60 days
+cat(sprintf("Time frame: 3 years\nRefit every 60 days\n"))
+fit.roll_60 = vector(mode = "list", length = length(models))
+names(fit.roll_60) = names(models)
+for (i in 1:length(models)) {
+  cat(sprintf("Fitting: %s\n", names(fit.roll_60[i])))
+  fit.roll_60[[i]] = ugarchroll(spec = models[[i]],
+                                data = r_ibov.xts,
+                                n.start = n.startForecast,
+                                refit.window = "recursive",
+                                refit.every = 60)
 }
 
 
-# Comparacao de Normal vs t-student in fit.roll
-tbl_temp = tbl %>% dplyr::filter(EstimationName == "fit.roll") %>% tidyr::pivot_wider(id_cols=c("fit"),
-                                                                                      names_from = "Restimation",
-                                                                                      names_prefix = "Rest_",
-                                                                                      names_repair = "check_unique",
-                                                                                      values_from = MSE)
-write.table(tbl_temp , file = "./Trabalho/Tables/Table1_MSE.csv")
-print(tbl_temp)
 
-tbl_temp = tbl %>% dplyr::filter(EstimationName == "fit.roll") %>% tidyr::pivot_wider(id_cols=c("fit"),
-                                                                                      names_from = "Restimation",
-                                                                                      names_prefix = "Rest_",
-                                                                                      names_repair = "check_unique",
-                                                                                      values_from = QL)
+# *************** MOVING WINDOW (3 year) ***************
+# Determina qual o periodo de reestimacao
+# Expanding window recalculation every 5 days
+cat(sprintf("Time frame: 3 years\nRefit every 5 days\n"))
+fit.mov3_5 = vector(mode = "list", length = length(models))
+names(fit.mov3_5) = names(models)
+for (i in 1:length(models)) {
+  cat(sprintf("Fitting: %s\n", names(fit.mov3_5[i])))
+  fit.mov3_5[[i]] = ugarchroll(spec = models[[i]],
+                               data = r_ibov.xts,
+                               n.start = n.startForecast,
+                               refit.window = "moving",
+                               refit.every = 5)
+}
 
-write.table(tbl_temp , file = "./Trabalho/Tables/Table1_QL.csv")
-print(tbl_temp)
+# Expanding window recalculation every 20 days
+cat(sprintf("Time frame: 3 years\nRefit every 20 days\n"))
+fit.mov3_20 = vector(mode = "list", length = length(models))
+names(fit.mov3_20) = names(models)
+for (i in 1:length(models)) {
+  cat(sprintf("Fitting: %s\n", names(fit.mov3_20[i])))
+  fit.mov3_20[[i]] = ugarchroll(spec = models[[i]],
+                                data = r_ibov.xts,
+                                n.start = n.startForecast,
+                                refit.window = "moving",
+                                refit.every = 20)
+}
 
-# Comparação do metodo expanding vs moving usando Normal
-tbl_temp = tbl %>% dplyr::filter(Distribution == "norm", Restimation ==5) %>% tidyr::pivot_wider(id_cols=c("ModelName"),
-                                                                                                 names_from = "EstimationName",
-                                                                                                 names_prefix = NULL,
-                                                                                                 names_repair = "check_unique",
-                                                                                                 values_from = MSE)
-
-write.table(tbl_temp , file = "./Trabalho/Tables/Table2_MSE.csv")
-print(tbl_temp)
-
-tbl_temp = tbl %>% dplyr::filter(Distribution == "norm", Restimation ==5) %>% tidyr::pivot_wider(id_cols=c("ModelName"),
-                                                                                                 names_from = "EstimationName",
-                                                                                                 names_prefix = NULL,
-                                                                                                 names_repair = "check_unique",
-                                                                                                 values_from = QL)
-write.table(tbl_temp , file = "./Trabalho/Tables/Table2_QL.csv")
-print(tbl_temp)
-
-# Comparação do metodo expanding vs moving usando t-Student
-tbl_temp = tbl %>% dplyr::filter(Distribution == "sstd", Restimation ==5) %>% tidyr::pivot_wider(id_cols=c("ModelName"),
-                                                                                                 names_from = "EstimationName",
-                                                                                                 names_prefix = NULL,
-                                                                                                 names_repair = "check_unique",
-                                                                                                 values_from = MSE)
-write.table(tbl_temp , file = "./Trabalho/Tables/Table2_MSE_sstd.csv")
-print(tbl_temp)
-
-tbl_temp = tbl %>% dplyr::filter(Distribution == "sstd", Restimation ==5) %>% tidyr::pivot_wider(id_cols=c("ModelName"),
-                                                                                                 names_from = "EstimationName",
-                                                                                                 names_prefix = NULL,
-                                                                                                 names_repair = "check_unique",
-                                                                                                 values_from = QL)
-write.table(tbl_temp , file = "./Trabalho/Tables/Table2_QL_sstd.csv")
-print(tbl_temp)
-
-# Salva a tabela completa em CSV
-write.table(tbl , file = "./Trabalho/Tables/Table3_overall.csv")
-print(tbl)
+# Expanding window recalculation every 60 days
+cat(sprintf("Time frame: 3 years\nRefit every 60 days\n"))
+fit.mov3_60 = vector(mode = "list", length = length(models))
+names(fit.mov3_60) = names(models)
+for (i in 1:length(models)) {
+  cat(sprintf("Fitting: %s\n", names(fit.mov3_60[i])))
+  fit.mov3_60[[i]] = ugarchroll(spec = models[[i]],
+                                data = r_ibov.xts,
+                                n.start = n.startForecast,
+                                refit.window = "moving",
+                                refit.every = 60)
+}
 
 
-
-
-
-
-
-
+# Save all models forecast
+save(fit.roll_5,
+     fit.roll_20,
+     fit.roll_60,
+     fit.mov3_5,
+     fit.mov3_20,
+     fit.mov3_60,
+     Ibov.data,
+     models,
+     file = "./Trabalho/Database/Garch_HF_Forecast.RData")
